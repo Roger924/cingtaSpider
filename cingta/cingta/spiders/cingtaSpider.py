5255c82181f266cdefe53e7fd52865e45b79a4c3
Roger924 <rwxren987@163.com>
2019-09-21 14:53:52 +0800

    Use bs4/re to parse the response

--- cingta/cingta/spiders/cingtaSpider.py
+++ cingta/cingta/spiders/cingtaSpider.py
@@ -1,9 +1,10 @@
 #coding:utf-8
 import scrapy
 from cingta.items import CingtaItem
-import time
 from scrapy.crawler import CrawlerProcess
+import bs4,re
 from goto import with_goto
+
 
 class cingtaSpider(scrapy.Spider):
 	name ='cingta'
@@ -15,12 +16,12 @@ class cingtaSpider(scrapy.Spider):
 	pageIndex=1
 	
 	# You should change the crawl max page number here
-	pageMax=3
+	pageMax=10
 	
 	def parse(self, response):
 			
-		for link in response.xpath('//*[@id="__layout"]/div/section/div[2]/div/div[1]/div/section/ul/a/@href').extract():
-			yield scrapy.Request('https://www.cingta.com'+link, callback=self.parse_details)
+		for link in bs4.BeautifulSoup(response.text).select('ul.contentList > a'):
+			yield scrapy.Request('https://www.cingta.com'+link.get('href'), callback=self.parse_details)
 
 		self.pageIndex += 1
 		next_url = self.url % self.pageIndex
@@ -30,58 +31,65 @@ class cingtaSpider(scrapy.Spider):
 	@with_goto	
 	def parse_details(self,response):
 		item = CingtaItem()
-		item['title'] = response.xpath('//*[@id="__layout"]/div/section/div[2]/div/div[1]/div/p/text()').get().strip()
+		
+		title = bs4.BeautifulSoup(response.text).select('p#titleId')
+		if len(title) > 0:
+			item['title'] = title[0].getText().strip()
+			
 		item['url'] = response.url
-		item['date'] = response.xpath("//span[@class='date']/text()").extract()
-		item['type'] = response.xpath("//span[@class='type']/text()").extract()
 		
+		type_idx = 0
+		for date_type in bs4.BeautifulSoup(response.text).select('span.bottom_left > span'):
+			detail_class = date_type.get('class')
+			if len(detail_class) > 0:
+				if detail_class[0] == 'date':
+					item['date'] = date_type.getText()
+				elif detail_class[0] == 'type':
+					if type_idx == 0:
+						item['source'] = date_type.getText()
+					elif type_idx == 1:
+						item['type'] = date_type.getText()
+					type_idx = type_idx+1
+					
+		if item['source'] != '青塔':
+			goto .end
+			
+		source = ''
+		source_parse = ''
+					
+		source_format0 = bs4.BeautifulSoup(response.text).select('p.source')
+		if len(source_format0) > 0:
+			source = source_format0[-1].getText()
+			if len(source) > 0:
+				source_parse = self.parse_source(source)
+				if len(source_parse) > 0:
+					item['source'] = source_parse
+					goto .end
 		
-		# Different detail pages have different methods for analysis. 
-		# I have crawled the data from 2019/1/1-2019/4/28 and only 370 items do not have source(Total num is 2092)
-		source_format1 = response.xpath('//*[@id="__layout"]/div/section/div[2]/div/div[1]/div/div[2]/p/text()').extract()
+		source_format1 = bs4.BeautifulSoup(response.text).select('div#contentId > p')
 		if len(source_format1) > 0:
-			if '来源' in source_format1[-1]:
-				item['source'] = source_format1[-1]
-				goto .end
-			elif len(source_format1) > 1 and '来源' in source_format1[-2]:
-				item['source'] = source_format1[-2]
-				goto .end
-				
-		source_format2 = response.xpath('//strong/text()').extract()
-		if len(source_format2) > 0:
-			if '来源' in source_format2[-1]:
-				item['source'] = source_format2[-1]
-				goto .end
-				
-		source_format3 = response.xpath('//span[@class="bjh-p"]/text()').extract()
-		if len(source_format3) > 0:
-			if '来源' in source_format3[-1]:
-				item['source'] = source_format3[-1]
-				goto .end
-
-		source_format4 = response.xpath('//*[@id="__layout"]/div/section/div[2]/div/div[1]/div/div[2]/div/text()').extract()
-		if len(source_format4) > 0:
-			if '来源' in source_format4[-1]:
-				item['source'] = source_format4[-1]
-				goto .end
-
-		source_format5 = response.xpath('//*[@id="__layout"]/div/section/div[2]/div/div[1]/div/div[2]/div/article/div/p/text()').extract()
-		if len(source_format5) > 0:
-			if '来源' in source_format5[-1]:
-				item['source'] = source_format5[-1]
-				goto .end
-
-		source_format6 = response.xpath('//*[@id="blog_article"]/p/text()').extract()
-		if len(source_format6) > 0:
-			if '来源' in source_format6[-1]:
-				item['source'] = source_format6[-1]
-				goto .end
-
-		source_format7 = response.xpath('//span[@class="bjh-strong"]/text()').extract()
-		if len(source_format7) > 0:
-			if '来源' in source_format7[-1]:
-				item['source'] = source_format7[-1]
-				goto .end
-		
+			source = source_format1[-1].getText()
+			if len(source) > 0:
+				source_parse = self.parse_source(source)
+				if len(source_parse) > 0:
+					item['source'] = source_parse
+					goto .end
+					
 		label .end
 		yield item
+
+	def parse_source(self, source):
+		result = ''
+		if '转载' in source:
+			source_regex = re.compile(r'转载自：(.*?)。.*')
+			regex_result = source_regex.search(source)
+			if regex_result:
+				result = regex_result[1]
+			elif '青塔网原创' in source:
+				result = '青塔'
+			else:
+				result = source
+		elif '来源' in source:
+			result = source	
+		return result
+		
